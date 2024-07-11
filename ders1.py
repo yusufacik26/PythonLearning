10997b2173dba575b0d9987577fd7e9186d2a001
Yusuf Açık <yusufacikedu@gmail.com>
2024-07-11 18:36:17 +0300

    Python listeler

---
+++ ders1.py
@@ -0,0 +1,13 @@
+yazi = "benim adim yusuf acik".split()
+print(yazi[1][2]) # ilk kısım kelime seçimi 
+                  # ikinci kısım ise kelime harfi seçimi 
+
+#sayilar = [1,2,3,4,5,6]
+#sonuc = sayilar[2]
+#sonuc = sayilar[4]
+
+listeYusuf = ["yusuf",20]
+listeNur = ["nur",21]
+
+listeOgrenci = [listeYusuf,listeNur]
+print(listeOgrenci)
