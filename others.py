e84463c4cabc5bfca4b5a0113185fdf20aee1271
Yusuf Açık <yusufacikedu@gmail.com>
2024-07-17 15:09:48 +0300

    Create others.py

---
+++ others.py
@@ -0,0 +1,21 @@
+#Identity operator : is
+
+x = y =[1,2,3,4]
+z = [1,2,3,4]
+
+#print(x == z )
+
+#print( x is y) # eşdeğerlik sağlar 
+
+
+
+
+# Membership  Operator : in 
+
+
+a = ["java","python"]
+
+#print("python" in a)
+
+email = "yusuf@gmail.com"
+print("@"in email)
