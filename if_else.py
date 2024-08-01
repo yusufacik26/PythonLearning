14db544ca5397cf02543a19aeb681e5768555e84
Yusuf Açık <yusufacikedu@gmail.com>
2024-08-01 19:08:00 +0300

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,34 @@
+
+x = 5
+
+"""if(x < 0):
+    print("sayi negatiftir")
+else:
+    print(" sayı pozitiftir")    
+
+
+giris = False
+
+if(giris):
+    print("merhaba")
+
+username = "yusuf"
+password = "1234"
+
+if(username == "yusuf") and (password == "1234"):
+    print("sisteme giris yapildi")
+else:
+    print("sisteme giris yapılmadı")  """
+
+username = "yusuf"
+password = "1234"
+
+if(username == "yusuf"):
+    if(password =="1234"):
+        print("sisteme girildi")
+    else:
+        print("şifreniz yanlış")    
+else:
+    print("kullanıcı adınız yanlış")
+            
+
