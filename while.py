44bd257bb36031c14e3b32a26924624d550608a6
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-08-01 20:48:08 +0300

    while örnek

---
+++ while.py
@@ -0,0 +1,19 @@
+i = 0 
+
+#while i<10:
+ #   print(i)
+  #  i += 1
+
+"""while i<100:
+    if(i%2 == 0):
+        print("çift sayı :",i)
+    else:
+        print("tek sayi :",i)
+    i += 1    """
+
+
+email = ""
+while not email:
+    email = input("bir email adresi giriniz :") 
+
+print("girdiğiniz e mail adresi :",email)
