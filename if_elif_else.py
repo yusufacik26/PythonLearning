8f7fba27adbd8bf7e77e106e2b930ede82301f00
Yusuf Açık <yusufacikedu@gmail.com>
2024-08-01 19:08:42 +0300

    if elif

---
+++ if_elif_else.py
@@ -0,0 +1,23 @@
+
+"""a = 10
+b = 10
+
+if(a<b):
+    print("b a dan büyük")
+elif(a == b):
+    print("a b ye eşit") 
+else:
+    print("a b den büyük")       
+
+"""
+karnenotu = 90
+
+if(karnenotu<50):
+    print("başarısız")
+elif(karnenotu>=50) and (karnenotu<70):
+    print("başarılı") 
+elif(karnenotu>=70) and (karnenotu<=85):
+    print("teşekkür belgesi")
+
+else:
+    print("taktir belgesi")
