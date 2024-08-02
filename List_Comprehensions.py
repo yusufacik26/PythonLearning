eb81775a0e623863863faad06ab231892809a09b
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-08-02 23:41:23 +0300

    List metod2

---
+++ List_Comprehensions.py
@@ -0,0 +1,25 @@
+sayilar = []
+
+"""for i in range(10):
+    sayilar.append(i)
+
+print(sayilar)   """ 
+
+liste = [3,4,5,6,7]
+#[exression for items in list]
+
+#sayilar2 = [i for i in range(10)]
+#sayilar3 = [i**2 for i in range(10)]
+#print(sayilar3)
+
+"""sayilar = [i*2 for i in liste]
+
+sonuc = [str(sayi) for sayi in liste]
+print(sonuc)"""
+
+
+isim = "yusuf"
+
+sonuc = [a.upper() for a in isim]
+
+print(sonuc)
