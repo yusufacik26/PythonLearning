392637c4446dd07cbae35d1b4a28380ed1b56521
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-08-08 14:49:26 +0300

    Create sum_around_func.py

---
+++ sum_around_func.py
@@ -0,0 +1,22 @@
+
+sayilar = [23,5,3,16,72]
+
+sonuc = sum(sayilar)
+
+
+
+
+
+urunler = [
+    {"title":"kitap a","price":15},
+     {"title":"kitap b","price":25},
+     {"title":"kitap c ","price":35}
+    ]
+toplamFiyat = sum(urun["price"] for urun in urunler)
+
+print(toplamFiyat)
+
+
+
+deger = round(32.54543)
+print(deger)
