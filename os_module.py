f9c80971c5589605e481375949d458c52df70b8e
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-08-11 17:24:57 +0300

    Create os_module.py

---
+++ os_module.py
@@ -0,0 +1,31 @@
+
+import os 
+sonuc = dir(os)
+
+sonuc = os.name
+
+
+
+# etkin dizi öğrenme
+
+sonuc = os.getcwd()
+
+
+# dizin değiştirme
+"""  os.chdir("..")   """
+
+# klasor oluşturma
+
+#os.makedirs("yeniklasör/deneme")    
+#os.rename("yeklasor","ustklasor")
+#os.rmdir("ustklasor")
+#os.removedirs("yeniklasor/deneme")
+
+
+
+#listeleme
+#sonuc = os.listdir("//users")
+#print(sonuc)
+
+
+
