6efab81f852d51c46f71970a47c4c44bf84eaa70
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-08-11 17:24:11 +0300

    Create error_types.py

---
+++ error_types.py
@@ -0,0 +1,29 @@
+#SytaxError == Yazım hatası
+
+#print("yusu"f)
+
+
+#NameError = var olmayan bir değişkeni çağırmaya çalışmak
+#PRİNT(İSİM)
+
+
+#ValueError
+# int("10a")
+#
+# 
+# 
+# IndexError --> yanlış index numarası
+# liste = ["yusuf"]
+# print(liste[1])
+
+# AttributeError --> olmayan bir özelliğe ulaşmak istediğimizde
+
+"merhaba".upper()
+
+
+
+
+# 
+# 
+#  
+ 
