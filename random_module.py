5ccb0729ebf4a010b2200442661b302370264dc9
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-08-11 17:25:09 +0300

    Create random_module.py

---
+++ random_module.py
@@ -0,0 +1,40 @@
+
+import random
+
+sonuc = dir(random)
+
+
+sonuc = int(random.uniform(0,10))
+
+
+sonuc = random.randint(1,200)  # integer sayı ürettiriyor
+
+users = ["yusuf","emin","mert","cenk"]
+sonuc1 = users[random.randint(0,len(users)-1)]
+
+
+
+
+
+liste = list(range(10))
+
+random.shuffle(liste)
+
+
+
+print(liste)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(sonuc1)
