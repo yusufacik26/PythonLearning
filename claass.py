6271dbf19e1af7a6c8562a17aa4bc93b09a41964
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-08-16 14:47:58 +0300

    Create claass.py

---
+++ claass.py
@@ -0,0 +1,30 @@
+
+#Class
+
+class Arac:
+    pass
+
+
+
+#Instance,Object
+
+Arac1 = Arac()
+
+Arac2= Arac()
+
+
+class Products:
+    pass
+    
+
+
+p1 = Products() #Telefon
+p2 = Products() #bilgisayar
+p3 = Products() #Kulaklık
+
+listProducts = [p1,p2,p3]
+
+
+for p in listProducts:
+    print(p)
+    
