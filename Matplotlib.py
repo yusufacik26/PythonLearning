8cba7d9730721cf1b037fd30a450d3ebc76a114f
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-09-09 00:12:56 +0300

    Create Matplotlib.py

---
+++ Matplotlib.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread("opencvlogo.png",0)
+
+plt.imshow(img,cmap="gray",interpolation="bicubic")
+
+
+
+plt.show()
