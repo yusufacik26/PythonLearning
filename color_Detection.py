e382aaf99b1b785012da413d2ebb3679b2779e5f
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-09-09 22:35:27 +0300

    Create color_Detection.py

---
+++ color_Detection.py
@@ -0,0 +1,21 @@
+
+import cv2
+from colorthief import ColorThief
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    _,image= camera.read()
+    cv2.imshow('image',image)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite('test1.jpg',image)
+camera.release()
+cv2.destroyAllWindows()
+
+
+def ss():
+    colortheif = ColorThief('test1.jpg')
+    colors = colortheif.get_palette(color_count=2)
+    print(colors)
+
+ss()
