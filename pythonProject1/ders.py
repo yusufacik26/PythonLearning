76bd407ff218d1405a72ff4f269e0f18a7f447c3
Yusuf Açık <147844446+yusufacik26@users.noreply.github.com>
2024-10-19 18:39:59 +0300

    Miuu notlar

---
+++ pythonProject1/ders.py
@@ -0,0 +1,22 @@
+print("hello ai era")
+
+print(9)
+
+
+# ####################### Değişken tipini öğrenme ################
+
+type("yusuf")
+
+type(3.32)
+
+
+# ####################### Değişken oluşturma ################
+
+a = 3
+b = 3
+
+
+print(a / b)
+
+### List Tupple  set ve dictionary  veri yapıları  aynı zamanda Python Collections(Arrays) olarak ifade edilmektedir
+
